d1fb9293b47fefa6497c6cb313d82c57fe311570
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-08-02 11:44:22 -0700

    Appetizer List View

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizer List View")
-                .navigationTitle("🍟Appetizers")
+            List(Mockdata.appetizers){ appetizer in
+               AppetizerListCell(appetizer: appetizer)
+            }
+            .listStyle(.plain)
+            .navigationTitle("🍟 Appetizers")
         }
     }
 }

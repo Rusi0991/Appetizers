b98a5acb4159564ea2e3348085cfb803728d77ad
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-08-01 22:16:00 -0700

    Appetizer Model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Ruslan Ismayilov on 8/1/23.
+//
+
+import Foundation
+
+struct Appetizer : Decodable {
+    let id : Int
+    let name :String
+    let description : String
+    let price : Double
+    let imageURL : String
+    let calories : Int
+    let protein :Int
+    let carbs : Int
+}
+
+struct AppetizerResponse {
+    let request : [Appetizer]
+}
+
+struct Mockdata {
+    static let sampleAppetizer = Appetizer(id: 001,
+                                           name: "Test app",
+                                           description: "This is test appetizer",
+                                           price: 7.99,
+                                           imageURL: "",
+                                           calories: 150,
+                                           protein: 20,
+                                           carbs: 90)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}

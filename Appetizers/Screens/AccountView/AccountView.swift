9118be56a1bc9c89fffb0ef5008207c03385f075
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-08-10 12:59:43 -0700

    AccountViewModel & Text Validation

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -9,30 +9,22 @@ import SwiftUI
 
 struct AccountView: View {
     
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var birthdate = Date()
-    @State private var extraNapkins = false
-    @State private var frequentRefills = false
-
-
-    
+    @StateObject var viewModel = AccountViewModel()
     var body: some View {
         NavigationView {
             Form{
                 Section(header: Text("Personal Info")){
-                    TextField("First Name", text: $firstName)
-                    TextField("Last Name", text: $lastName)
-                    TextField("Email", text: $email)
+                    TextField("First Name", text: $viewModel.firstName)
+                    TextField("Last Name", text: $viewModel.lastName)
+                    TextField("Email", text: $viewModel.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
                     
-                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                    DatePicker("Birthdate", selection: $viewModel.birthdate, displayedComponents: .date)
                     
                     Button{
-                        print("tapped")
+                        viewModel.saveChanges()
                     } label: {
                         Text("Save Changes")
 //                            .foregroundColor(.brandPrimary)
@@ -40,14 +32,18 @@ struct AccountView: View {
 
                 }
                 Section(header: Text("Requests")){
-                    Toggle("Extra Napkins", isOn: $extraNapkins)
-                    Toggle("Frequen Refills", isOn: $frequentRefills)
+                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
+                    Toggle("Frequen Refills", isOn: $viewModel.frequentRefills)
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
 
                         }
                     .navigationTitle("👨‍💼Account")
-            
+        }
+        .alert(item: $viewModel.alertItem){ alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
